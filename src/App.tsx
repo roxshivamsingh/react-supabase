b01bfe2e557a3bd7c4e2616dba3111f54a428646
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-08 18:33:24 +0530

    ADD: Get records function;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
+import supabase from "./supabase"
 
+type TValue = { id: number, name: string }
+type TCorpus = { value: TValue[], loading: boolean }
+const INIT_CORPUS: TCorpus = { value: [], loading: true }
 function App() {
+
+  const [corpus, setCorpus] = useState<TCorpus>(INIT_CORPUS)
+  const getCountries = useCallback(async () => {
+    try {
+      const { data } = await supabase.from("countries").select()
+      setCorpus((prev) => ({ ...prev, value: data as TValue[] }))
+
+    } catch {
+      console.log("No rows.")
+    } finally {
+      setCorpus((prev) => ({ ...prev, loading: false }))
+    }
+  }, [])
+
+  useEffect(() => { getCountries() }, [getCountries])
+
+  console.log(corpus)
 
   return (
     <>
